ae75e219bfaf9f8fb9b49986cb03e9fbf4fc9be3
Robert Saunders Jr <rsaunders138@hotmail.com>
2024-02-07 16:36:07 -0600

    updates

--- src/routes/tictactoe/+page.svelte
+++ src/routes/tictactoe/+page.svelte
@@ -1,49 +1,52 @@
 <script lang="ts">
 </script>
 
-<h1 class=" text-4xl flex items-center justify-center">Tic Tac Toe</h1>
+<h1 class=" flex items-center justify-center text-4xl">Tic Tac Toe</h1>
 
 <section class=" mt-40 p-5">
-	<div class="flex justify-center relative">
+	<div class="relative flex justify-center">
 		<div class="board grid grid-cols-3" id="board">
 			<div
 				id="0"
-				class="cell w-20 h-20 md:w-32 md:h-32 text-3xl flex items-center justify-center"
+				class="cell flex h-20 w-20 items-center justify-center text-3xl md:h-32 md:w-32"
 			></div>
 			<div
 				id="1"
-				class="cell border-x border-black dark:border-white w-20 h-20 md:w-32 md:h-32 text-3xl flex items-center justify-center"
+				class="cell flex h-20 w-20 items-center justify-center border-x border-black text-3xl dark:border-white md:h-32 md:w-32"
 			></div>
 			<div
 				id="2"
-				class="cell w-20 h-20 md:w-32 md:h-32 text-3xl flex items-center justify-center"
+				class="cell flex h-20 w-20 items-center justify-center text-3xl md:h-32 md:w-32"
 			></div>
 			<div
 				id="3"
-				class="cell border-y border-black dark:border-white w-20 h-20 md:w-32 md:h-32 text-3xl flex items-center justify-center"
+				class="cell flex h-20 w-20 items-center justify-center border-y border-black text-3xl dark:border-white md:h-32 md:w-32"
 			></div>
 			<div
 				id="4"
-				class="cell border border-black dark:border-white w-20 h-20 md:w-32 md:h-32 text-3xl flex items-center justify-center"
+				class="cell flex h-20 w-20 items-center justify-center border border-black text-3xl dark:border-white md:h-32 md:w-32"
 			></div>
 			<div
 				id="5"
-				class="cell border-y border-black dark:border-white w-20 h-20 md:w-32 md:h-32 text-3xl flex items-center justify-center"
+				class="cell flex h-20 w-20 items-center justify-center border-y border-black text-3xl dark:border-white md:h-32 md:w-32"
 			></div>
 			<div
 				id="6"
-				class="cell w-20 h-20 md:w-32 md:h-32 text-3xl flex items-center justify-center"
+				class="cell flex h-20 w-20 items-center justify-center text-3xl md:h-32 md:w-32"
 			></div>
 			<div
 				id="7"
-				class="cell border-x border-black dark:border-white w-20 h-20 md:w-32 md:h-32 text-3xl flex items-center justify-center"
+				class="cell flex h-20 w-20 items-center justify-center border-x border-black text-3xl dark:border-white md:h-32 md:w-32"
 			></div>
 			<div
 				id="8"
-				class="cell w-20 h-20 md:w-32 md:h-32 text-3xl flex items-center justify-center"
+				class="cell flex h-20 w-20 items-center justify-center text-3xl md:h-32 md:w-32"
 			></div>
 		</div>
 
 		<div id="win-line"></div>
 	</div>
 </section>
+
+<style>
+</style>
